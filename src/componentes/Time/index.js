73b8e8e250393396ccac27ed4675c536131d26bd
Guilherme Santos <guiss.devs@gmail.com>
2023-07-27 21:35:24 -0300

    Finalizando

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,10 +2,10 @@ import Colaborador from '../Colaborador'
 import hexToRgba from 'hex-to-rgba'
 import './time.css'
 
-const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.cor} type='color' className='input-cor'/>
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor'/>
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
@@ -14,6 +14,7 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
                         colaborador={colaborador}
                         corDeFundo={time.cor}
                         aoDeletar={aoDeletar}
+                        aoFavoritar={aoFavoritar}
                     />
                 })}
             </div>
